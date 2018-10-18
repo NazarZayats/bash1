d2e8e14af53403176270488bd61ab70351636f5c
Bohdan Hamulets <bogdan.robota@gmail.com>
2018-10-18 17:14:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Install java_jdk_1.8.0-45 + TomCat + Maven + MariaDB + configure and deployment OMS app on centos_7 
 # need ROOT + WGET + FTP 
 #
+yum -y update
 yum -y install wget
 # Java #######################################
 cd /opt/
